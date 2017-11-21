c4880946fcb368372417f839df431f2eb86a21e1
Juraj Andrássy <juraj.andrassy@gmail.com>
2017-11-21 21:26:55 +0100

    EspProxy automatic flashing mode; led on indicates flashing mode

--- examples/tools/EspProxy/EspProxy.ino
+++ examples/tools/EspProxy/EspProxy.ino
@@ -4,12 +4,16 @@
 #define BAUD 115200L
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  
   Serial.begin(BAUD);
 
 #ifdef FLASHING
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial1.begin(BAUD * 2); //double speed is necessary, but it disturbs frequency test of the 'download tool'
   Serial1.resetESP(true); // reset to bootloader. no need to push the B/L button while connecting to USB
 #else
+  digitalWrite(LED_BUILTIN, LOW);
   Serial1.begin(BAUD);
   Serial1.resetESP(); // power cycle
 #endif
@@ -17,9 +21,34 @@ void setup() {
 
 void loop() {
   while (Serial.available()) {
+    detectFlashing();
     Serial1.write(Serial.read());
   }
   while (Serial1.available()) {
     Serial.write(Serial1.read());
+  }
+}
+
+const byte syncFrame[] = {0xC0, 0x00, 0x08, 0x24, 0x00, 0xDD, 0x00, 0x00, 0x00, 0x07, 0x07, 0x12, 0x20};
+const byte checkSumPos = 5; // esptool.py and FDT do not send the checksum for sync frame, IDE does
+byte syncFrameIndex = 0;
+boolean detectSyncFrame = true;
+
+void detectFlashing() {
+  if (!detectSyncFrame)
+    return;
+  byte b = Serial.peek();
+  if (!(b == syncFrame[syncFrameIndex] || (syncFrameIndex == checkSumPos && b == 0x00))) {
+    syncFrameIndex = 0;
+  } else {
+    syncFrameIndex++;
+    if (syncFrameIndex == sizeof(syncFrame)) {
+      detectSyncFrame = false;
+      Serial1.end();
+      Serial1.resetESP(true); // reset to bootloader
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial1.begin(BAUD * 2); //double speed is necessary, but it disturbs frequency test of the 'download tool'
+      Serial1.write(syncFrame, sizeof(syncFrame) - 1); // last byte was not read, only peek
+    }
   }
 }
